b246a000a4035cf2e0cd1c8387493494a9c5fe6a
Raoul DIFFOUO <diraulo@gmail.com>
2015-03-17 16:02:46 +0200

    enable livereload for development

--- config.rb
+++ config.rb
@@ -2,6 +2,10 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+configure :development do
+  activate :livereload
+end
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
